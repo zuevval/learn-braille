4fad46ca3b35eaf10a564b9b2e24c6432981744e
zuevval <valera.zuev.zva@gmail.com>
2021-01-26 21:11:58 +0300

    #291 log all accessibility events to debug serial

--- app/src/main/java/com/github/braillesystems/learnbraille/ui/screens/MainActivity.kt
+++ app/src/main/java/com/github/braillesystems/learnbraille/ui/screens/MainActivity.kt
@@ -2,6 +2,9 @@ package com.github.braillesystems.learnbraille.ui.screens
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.accessibility.AccessibilityEvent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -9,6 +12,17 @@ import androidx.navigation.ui.NavigationUI
 import com.github.braillesystems.learnbraille.R
 import com.github.braillesystems.learnbraille.ui.brailletrainer.BrailleTrainer
 import timber.log.Timber
+
+class MyAccessibilityDelegate : View.AccessibilityDelegate() {
+    override fun onRequestSendAccessibilityEvent(
+        host: ViewGroup?,
+        child: View?,
+        event: AccessibilityEvent?
+    ): Boolean {
+        Timber.i("Event: %s", event.toString())
+        return super.onRequestSendAccessibilityEvent(host, child, event)
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +33,9 @@ class MainActivity : AppCompatActivity() {
 
         Timber.i("onCreate")
         setContentView(R.layout.activity_main)
+
+        val contentView = findViewById<View>(android.R.id.content)
+        contentView.accessibilityDelegate = MyAccessibilityDelegate()
 
         navController = findNavController(R.id.navHostFragment)
         NavigationUI.setupActionBarWithNavController(this, navController)
